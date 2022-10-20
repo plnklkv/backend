f7743377695fe067e098c652d4ecd97b2658b827
plnklkv <polinakulakova04072@gmail.com>
2022-10-20 20:56:20 +0700

    Создала проект, добавила класс User

---
+++ backend/DataContext.cs
@@ -0,0 +1,14 @@
+﻿global using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ backend/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string SurnName { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string MiddleName { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [MinLength(6)] 
+        [Required]
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
